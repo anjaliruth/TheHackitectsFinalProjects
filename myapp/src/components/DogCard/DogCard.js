32554805f0f193f2530d095318c35ec78081012a
lucymorgie <l_j_morgan5@hotmail.com>
2023-06-08 14:31:11 +0100

    dogcard update

--- myapp/src/components/DogCard/DogCard.js
+++ myapp/src/components/DogCard/DogCard.js
@@ -1,7 +1,15 @@
 import React from "react";
+import fridaImage from "../../Media/Frida/WhatsApp Image 2023-06-03 at 12.50.29.jpeg";
+import georgeImage from "../../Media/George/WhatsApp Image 2023-06-03 at 11.20.07.jpeg";
+import maisieImage from "../../Media/Maisie/WhatsApp Image 2023-06-03 at 11.15.30.jpeg";
+import millieImage from "../../Media/Millie/Screenshot 2023-06-05 at 11.52.23.png";
+import neilImage from "../../Media/Neil/Screenshot 2023-06-05 at 12.24.23.png";
+import sookieImage from "../../Media/Sookie/Screenshot 2023-06-08 at 10.48.30.png";
+
 const dogData = [
   {
     id: 1,
+    photo: fridaImage,
     name: "Frida",
     breed: "Pug",
     location: "London",
@@ -10,14 +18,16 @@ const dogData = [
   },
   {
     id: 2,
+    photo: georgeImage,
     name: "George",
-    breed: "Mixed",
+    breed: "Mixed Breed",
     location: "Manchester",
     sex: "Male",
     age: 4,
   },
   {
     id: 3,
+    photo: maisieImage,
     name: "Maisie",
     breed: "Springer Spaniel",
     location: "Birmingham",
@@ -26,6 +36,7 @@ const dogData = [
   },
   {
     id: 4,
+    photo: millieImage,
     name: "Millie",
     breed: "Lurcher",
     location: "London",
@@ -34,6 +45,7 @@ const dogData = [
   },
   {
     id: 5,
+    photo: neilImage,
     name: "Neil",
     breed: "Greyhound",
     location: "Leicester",
@@ -42,6 +54,7 @@ const dogData = [
   },
   {
     id: 6,
+    photo: sookieImage,
     name: "Sookie",
     breed: "Schnauzer",
     location: "Newcastle",
@@ -56,6 +69,7 @@ export default function DogCard() {
 {/* image, name, breed, location, sex, age, button */}
 {dogData.map((info, index)=>
 <div key={index}>
+<img src={info.photo} alt="dog photo" />
   <h2>{info.name}</h2>
   <p>{info.breed}</p>
   <p>{info.sex}</p>
