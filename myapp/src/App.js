f57f5815e65e87d0fd0d896e3b3a0f052459f905
Katim Touray <katim70@hotmail.co.uk>
2023-06-08 16:24:24 +0100

    navbar

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar.js'
+
+
 
 function App() {
   return (
     <div className="App">
-    
+    <NavBar/>
     </div>
   );
 }
