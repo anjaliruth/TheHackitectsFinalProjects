9257ce29f6fd01e0e2379dc66be5efca4a37b388
anjaliruth <ruthanjali0211@gmail.com>
2023-06-09 17:19:49 +0800

    moving aboutUs here

--- myapp/src/components/AboutUs/AboutUs.js
+++ myapp/src/components/AboutUs/AboutUs.js
@@ -10,17 +10,20 @@ export default function AboutUs() {
     <h1>About Us</h1>
 
 
-<div><p>At Paws For Affection, our mission is to connect shelters and potential adopters through our compassionate marketplace, facilitating the adoption process for dogs in need. With our innovative web app, we provide a database of available dogs, making it easier for adopters to find their perfect companion.</p></div>
+<div><p>At Paws For Affection, our mission is to connect shelters and potential adopters through our compassionate marketplace, facilitating the adoption process for dogs in need. With our innovative web app, we provide a database of available dogs, making it easier for adopters to find their perfect companion.</p>
 
 <img src={sookieImage} alt="picture of a dog"/>
+</div>
 
-<div><p>We are dedicated to ensuring that every adoption is a success. Our rigorous vetting process allows us to identify qualified adopters who can provide a safe and nurturing environment for their new pet. Once approved, we refer them to their chosen shelter, reducing the burden on shelters and increasing the likelihood of successful matches.</p></div>
+<div><p>We are dedicated to ensuring that every adoption is a success. Our rigorous vetting process allows us to identify qualified adopters who can provide a safe and nurturing environment for their new pet. Once approved, we refer them to their chosen shelter, reducing the burden on shelters and increasing the likelihood of successful matches.</p>
 
 <img src={fridaImage} alt="picture of a dog"/>
+</div>
 
-<div><p>Our user-friendly platform minimises unnecessary visits to shelters, saving time and resources for both adopters and shelters. By streamlining the adoption process, we create a more efficient and enjoyable experience for everyone involved.</p></div>
+<div><p>Our user-friendly platform minimises unnecessary visits to shelters, saving time and resources for both adopters and shelters. By streamlining the adoption process, we create a more efficient and enjoyable experience for everyone involved.</p>
 
 <img src={georgeImage} alt="picture of a dog"/>
+</div>
 
 <p>At Paws For Affection, we are committed to improving the lives of shelter dogs and promoting responsible pet ownership. Through technology and compassion, we aim to make a positive and lasting impact in the world of pet adoption. Join us in building a community that fosters lifelong bonds between dogs and their adoptive families.</p>
 </div>
