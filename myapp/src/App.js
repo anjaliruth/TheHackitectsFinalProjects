29cea8d16ef3fed3daf036b1e697879b7e1d5b63
lucymorgie <l_j_morgan5@hotmail.com>
2023-06-09 10:09:32 +0100

    initial start

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import DogCard from './components/DogCard/DogCard.js'
+import AboutUs from './components/AboutUs/AboutUs.js'
 
 function App() {
   return (
     <div className="App">
     <DogCard/>
+    <AboutUs/>
     </div>
   );
 }
