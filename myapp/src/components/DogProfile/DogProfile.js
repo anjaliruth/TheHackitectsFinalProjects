ababebf927d3ea7e76b0222eb833c1bdbfced523
K-velvet-P <123256886+K-velvet-P@users.noreply.github.com>
2023-06-09 15:37:30 +0100

    added dog infomation

--- myapp/src/components/DogProfile/DogProfile.js
+++ myapp/src/components/DogProfile/DogProfile.js
@@ -1,5 +1,13 @@
 import PhotoCarousel from "./PhotoCarousel.js";
 import "./DogProfile.css";
+import { BsGenderAmbiguous } from 'react-icons/bs';
+import { FiClock } from 'react-icons/fi';
+import { FaDog } from 'react-icons/fa';
+import { BsFillBarChartFill } from 'react-icons/bs';
+import { FaMapMarkedAlt } from 'react-icons/fa';
+
+
+
 
 function DogProfile() {
   return (
@@ -9,6 +17,22 @@ function DogProfile() {
         <p>Placeholder text</p>
         <p>Placeholder text</p>
       </div>
+
+      <div className="buttons">
+      <button className="button1">Adopt Me!</button>
+      <button className="button2">How To Care For Me</button>
+      </div>
+
+      <div className="dogProfile">
+        <p>ID</p>
+        <h1>Dog Name: "props"</h1>
+        <p> <BsGenderAmbiguous/> Gender: "props"</p>
+        <p> <FiClock/> Age: "props"</p>
+        <p><FaDog/><FaDog/> Size: "props"</p>
+        <p><BsFillBarChartFill/> Activity Level: "props"</p>
+        <p><FaMapMarkedAlt/> Location: "props"</p>
+        </div>
+
     </div>
   );
 }
