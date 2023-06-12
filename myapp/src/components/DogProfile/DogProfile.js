f88a0df8d5e747c47aa541816d1912f772edd9de
K-velvet-P <123256886+K-velvet-P@users.noreply.github.com>
2023-06-12 11:38:13 +0100

    continud the css on the dog profile

--- myapp/src/components/DogProfile/DogProfile.js
+++ myapp/src/components/DogProfile/DogProfile.js
@@ -1,40 +1,58 @@
 import PhotoCarousel from "./PhotoCarousel.js";
 import "./DogProfile.css";
-import { BsGenderAmbiguous } from 'react-icons/bs';
-import { FiClock } from 'react-icons/fi';
-import { FaDog } from 'react-icons/fa';
-import { BsFillBarChartFill } from 'react-icons/bs';
-import { FaMapMarkedAlt } from 'react-icons/fa';
-// AHAHAAHAHAAHAHAHAHAAHA
-
-
+import { BsGenderAmbiguous } from "react-icons/bs";
+import { FiClock } from "react-icons/fi";
+import { FaDog } from "react-icons/fa";
+import { BsFillBarChartFill } from "react-icons/bs";
+import { FaMapMarkedAlt } from "react-icons/fa";
 
 function DogProfile() {
   return (
-    <div>
-      <PhotoCarousel />
-      <div className="bannerTagLine">
-        <p>Placeholder text</p>
-        <p>Placeholder text</p>
-      </div>
+   
+    <div className="dogProfile">
 
-      <div className="buttons">
-      <button className="button1">Adopt Me!</button>
-      <button className="button2">How To Care For Me</button>
-      </div>
+      <div className="dogphotos">
+      
+        <div className="carousel">
+          <PhotoCarousel />
+          </div>
 
-      <div className="dogProfile">
-        <p>ID</p>
-        <h1>Dog Name: "props"</h1>
-        <p> <BsGenderAmbiguous/> Gender: "props"</p>
-        <p> <FiClock/> Age: "props"</p>
-        <p><FaDog/><FaDog/> Size: "props"</p>
-        <p><BsFillBarChartFill/> Activity Level: "props"</p>
-        <p><FaMapMarkedAlt/> Location: "props"</p>
+          <div className="bannerTagLine">
+            <p>Placeholder text</p>
+            <p>Placeholder text</p>
+          </div>
+
+          <div className="buttons">
+            <button className="button1">Adopt Me!</button>
+            <button className="button2">How To Care For Me</button>
         </div>
-        
 
+      </div>
+
+        <div className="dogInfo">
+          <p>ID</p>
+          <h1>Dog Name: "props"</h1>
+          <p>
+            {" "}
+            <BsGenderAmbiguous /> Gender: "props"
+          </p>
+          <p>
+            {" "}
+            <FiClock /> Age: "props"
+          </p>
+          <p>
+            <FaDog />
+            <FaDog /> Size: "props"
+          </p>
+          <p>
+            <BsFillBarChartFill /> Activity Level: "props"
+          </p>
+          <p>
+            <FaMapMarkedAlt /> Location: "props"
+          </p>
+        </div>
     </div>
+  
   );
 }
 
