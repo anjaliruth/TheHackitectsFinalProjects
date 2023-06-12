e70092ac6be19dc155b8b71d700a5c280921946c
thomasgrogan <thomasmgrogan1@gmail.com>
2023-06-12 15:46:47 +0100

    Homepage progress

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,19 +1,17 @@
-
-import logo from './logo.svg';
-import './App.css';
-import DogCard from './components/DogCard/DogCard.js'
-import AboutUs from './components/AboutUs/AboutUs.js'
+import logo from "./logo.svg";
+import "./App.css";
+import DogCard from "./components/DogCard/DogCard.js";
+import AboutUs from "./components/AboutUs/AboutUs.js";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
-import HomePage from "./components/HomePage/HomePage.js"
+import HomePage from "./components/HomePage/HomePage.js";
 
 function App() {
   return (
     <div className="App">
-
       <NavBar />
-    <DogCard/>
-    <AboutUs/>
+      <DogCard />
+      <AboutUs />
     </div>
   );
 }
