946afd989551ef3a2410caefb244cf6fb7e308b5
anjaliruth <ruthanjali0211@gmail.com>
2023-06-12 19:32:26 +0800

    changed to section

--- myapp/src/components/HomePage/HomePage.js
+++ myapp/src/components/HomePage/HomePage.js
@@ -3,10 +3,12 @@ import homePage from "../../Media/PFAHomePage.jpg"
 import "./HomePage.css"
 export default function HomePage() {
   return (
-    
+    <>
+    <section className="section1">
     <div className="homePage">
       <h2 className="homePageTitle">Paws For Affection, Your Companion in Adoptiong a Fur-ever friend.</h2>
-    <img src={homePage} alt=" girl holding dog on leash" className="homepagePic"/>
     </div>
+    </section>
+    </>
   )
 }
