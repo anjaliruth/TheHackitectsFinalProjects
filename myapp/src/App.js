9ee2a2742988ddfb185d7de0720a6136d979636c
Kyle <kylejordanwilliams@gmail.com>
2023-06-13 10:45:42 +0100

    navbar works

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import DogCard from "./components/DogCard/DogCard";
+import AboutUs from "./components/AboutUs/AboutUs";
+import InfoPack from "./components/InfoPack/InfoPack";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<DogCard />} />
+        <Route path="about-us" element={<AboutUs />} />
+        <Route path="info-pack" element={<InfoPack />} />
         {/* Add more Route components for other paths */}
       </Routes>
     </div>
--- myapp/src/components/InfoPack/InfoPack.js
+++ myapp/src/components/InfoPack/InfoPack.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function InfoPack() {
   return (
-    <div>InfoPack</div>
-  )
+    <div>
+      <h1>INFO</h1>
+    </div>
+  );
 }
