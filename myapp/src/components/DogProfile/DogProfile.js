da4635ea556fa86ec901af490bd2771d0a523cfb
anjaliruth <ruthanjali0211@gmail.com>
2023-06-14 23:20:26 +0800

    kinda styled dogProfile

--- myapp/src/components/DogProfile/DogProfile.js
+++ myapp/src/components/DogProfile/DogProfile.js
@@ -8,47 +8,45 @@ import { BsFillBarChartFill } from "react-icons/bs";
 import { FaMapMarkedAlt } from "react-icons/fa";
 import SwiperComp from "../Swiper/Swiper.js";
 
-
-function DogProfile({dogData}) {
-  const  {id} = useParams()
+function DogProfile({ dogData }) {
+  const { id } = useParams();
   return (
-    <div className="totalDogInfo" >
-    <div className="offsetdiv">   </div>
-    <div className="Swiper">
-    <SwiperComp/>
-    </div>
-
-     
-
-      <div className="dogInfo">
-      <div className="banner">
-            <p>Placeholder text</p>
-            <p>Placeholder text</p>
-          
-            <button className="button1">Adopt Me!</button>
-            <button className="button2">How To Care For Me</button>
-        </div>
-        <h1>Dog Name: {dogData[id-1].name}</h1>
-        <p >{dogData[id-1].id}</p>
-        <p> <BsGenderAmbiguous/> Gender: {dogData[id-1].sex}</p>
-        <p> <FiClock/> Age: {dogData[id-1].age}</p>
-        {/* <p><FaDog/><FaDog/> Size: {info.}</p> */}
-        {/* <p><BsFillBarChartFill/> Activity Level: "props"</p> */}
-        <p><FaMapMarkedAlt/> Location: {dogData[id-1].location}</p>
-        </div>
-        {/* ))} */}
-        
-
-      <div className="dogphotos">
-      
-      
-
-          
-
-
+    <div className="totalDogInfo">
+      <div className="offsetdiv"> </div>
+      <div className="swiper">
+        <SwiperComp />
       </div>
 
-        {/* <div className="dogInfo">
+      <div className="dogInfo">
+        <div className="dogProfileButtons">
+          <button className="button1">Adopt Me!</button>
+          <button className="button2">How To Care For Me</button>
+        </div>
+        <div className="dogStats">
+          <h1>{dogData[id - 1].name}</h1>
+          <div className="leftAlignDogInfo">
+            <p>ID #:{dogData[id - 1].id}</p>
+            <p>
+              {" "}
+              <BsGenderAmbiguous /> Gender: {dogData[id - 1].sex}
+            </p>
+            <p>
+              {" "}
+              <FiClock /> Age: {dogData[id - 1].age}
+            </p>
+            {/* <p><FaDog/><FaDog/> Size: {info.}</p> */}
+            {/* <p><BsFillBarChartFill/> Activity Level: "props"</p> */}
+            <p>
+              <FaMapMarkedAlt /> Location: {dogData[id - 1].location}
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* ))} */}
+
+      <div className="dogphotos"></div>
+
+      {/* <div className="dogInfo">
           <p>ID</p>
           <h1>Dog Name: "props"</h1>
           <p>
@@ -71,7 +69,6 @@ function DogProfile({dogData}) {
           </p>
         </div> */}
     </div>
-  
   );
 }
 
