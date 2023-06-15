2801d74cbf64035cd2149e056e8e8f521ec654ed
thomasgrogan <thomasmgrogan1@gmail.com>
2023-06-15 16:59:10 +0100

    adjusting sub-headings & paragraphs on the timeline, nearly there

--- myapp/src/components/ApplicationForm/ApplicationForm.js
+++ myapp/src/components/ApplicationForm/ApplicationForm.js
@@ -19,26 +19,30 @@ export default function ApplicationForm() {
           border="0"
         />
         <div className="step-rows">
-          <span className="step1">
+          <h5>Fill out Application Form</h5>
+          <span className="step1" id="application-parapgraph1">
             Use the application form to register your interest. Our team will
             read your application and see if your lifestyle matches the needs of
-            the dog.{" "}
+            the dog.
           </span>
           <br />
+          <h5 className="para-title">We'll arrange an appointment</h5>
           <span className="step1">
             If you and the dog seem well suited, we may arrange a virtual home
-            check or an appointment for you to come and meet them at the centre.{" "}
+            check or an appointment for you to come and meet them at the centre.
           </span>
           <br />
+          <h5 className="para-title">Perfect Match</h5>
           <span className="step1">
             If we mutually decide you're a perfect match, we'll arrange an
-            appointment for you to take them to their forever home.{" "}
+            appointment for you to take them to their forever home.
           </span>
           <br />
+          <h5>Supporting</h5>
           <span className="step1">
             We'll keep in touch to see how you and the dog are doing. If you
             need any advice or support, we're just a phone call away or you can
-            browse our dog advice section.{" "}
+            browse our dog advice section.
           </span>
         </div>
       </section>
