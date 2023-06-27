e19b6c0ccfa1ba8c346ede9af20bfd5c31e549e2
Kyle <kylejordanwilliams@gmail.com>
2023-06-27 14:05:41 +0100

    working portal

--- myapp/src/components/SubmitForAdoption/Login.js
+++ myapp/src/components/SubmitForAdoption/Login.js
@@ -7,6 +7,7 @@ import SubmitForAdoption from "./SubmitForAdoption";
 import "./Login.css";
 
 export default function Login() {
+  const [userId, setUserId] = useState(null);
   const [session, setSession] = useState(null);
 
   useEffect(() => {
@@ -18,6 +19,8 @@ export default function Login() {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session);
+      setUserId(session?.user.id);
+      console.log(userId);
     });
 
     return () => subscription.unsubscribe();
@@ -27,9 +30,12 @@ export default function Login() {
     return (
       <div className="totalSignup">
         <div className="dummyDiv"></div>
-     <h1 className="moreInfo">Sign up as a shelter. Once you have been verified as a partner, you will be able to list your organization's dogs for adoption!</h1>
+        <h1 className="moreInfo">
+          Sign up as a shelter. Once you have been verified as a partner, you
+          will be able to list your organization's dogs for adoption!
+        </h1>
         <div className="SupabaseLogin">
-        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+          <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
         </div>
         <div className="demoCredentials">
           <p>Test Email:</p>
@@ -40,7 +46,7 @@ export default function Login() {
   } else {
     return (
       <div>
-        <SubmitForAdoption />
+        <SubmitForAdoption user={userId} />
       </div>
     );
   }
